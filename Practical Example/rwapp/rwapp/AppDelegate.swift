e2dbf725d330ebd341fb8aba5caf5dbc5f0015c6
Julio Banda <julio.banda.castillo@bbva.com>
2021-08-30 13:32:45 -0500

    initial commit

---
+++ Practical Example/rwapp/rwapp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  rwapp
+//
+//  Created by Julio Banda on 30/08/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
